263902a534ab489fa6a6c70cc3cf53221c82517b
vunsh <joe.poeawesome@gmail.com>
2022-10-11 10:48:44 -0700

    changed api for weather

--- src/main/java/com/nighthawk/spring_portfolio/mvc/weather/WeatherApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/weather/WeatherApiController.java
@@ -7,7 +7,6 @@ import java.net.http.HttpResponse;
 import java.util.Date;
 import java.util.HashMap;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.http.HttpStatus;
@@ -21,7 +20,7 @@ public class WeatherApiController {
     private HttpStatus status; //last run status
     String last_run = null; //last run day of month
 
-    @GetMapping("/sd")   //added to end of prefix as endpoint
+    @GetMapping("/sandiego")   //added to end of prefix as endpoint
     public ResponseEntity<JSONObject> getSD() {
         //calls API once a day, sets body and status properties
         String today = new Date().toString().substring(0,10); 
@@ -31,9 +30,7 @@ public class WeatherApiController {
 
                 //RapidAPI header
                 HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://us-weather-by-zip-code.p.rapidapi.com/getweatherzipcode?zip=92127"))
-                .header("X-RapidAPI-Key", "33724f3274msh05d93db081cb062p14a3bajsnf355149933ab")
-                .header("X-RapidAPI-Host", "us-weather-by-zip-code.p.rapidapi.com")
+                .uri(URI.create("https://api.weatherbit.io/v2.0/current?lat=33.0167&lon=-117.1115&key=17270180ead94b27a3e874f0cdd513ec"))
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
 
@@ -43,19 +40,19 @@ public class WeatherApiController {
                 this.body = (JSONObject) new JSONParser().parse(response.body());
                 this.status = HttpStatus.OK;  //200 success
                 this.last_run = today;
+                //get the temp from body
             }
             catch (Exception e) {  //capture failure info
                 HashMap<String, String> status = new HashMap<>();
                 status.put("status", "RapidApi failure: " + e);
-
                 //Setup object for error
-                this.body = new JSONObject(status);
                 this.status = HttpStatus.INTERNAL_SERVER_ERROR; //500 error
                 this.last_run = null;
             }
         }
 
         //return JSONObject in RESTful style
+        System.out.println(body);
         return new ResponseEntity<>(body, status);
     }
 }
