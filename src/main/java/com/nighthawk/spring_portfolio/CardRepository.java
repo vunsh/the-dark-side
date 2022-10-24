46be150ca77da395c50da53ab5cdf60361eab7fe
Tigran7 <89666148+Tigran7@users.noreply.github.com>
2022-10-24 11:13:01 -0700

    Update CardRepository.java

--- src/main/java/com/nighthawk/spring_portfolio/CardRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/CardRepository.java
@@ -1,4 +1,4 @@
-package com.cs.learn.flashcard;
+package com.nighthawk.spring_portfolio;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
