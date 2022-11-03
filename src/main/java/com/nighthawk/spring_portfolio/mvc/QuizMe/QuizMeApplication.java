1e4774141957cf7ce7854d9eabe5663057d3b1e8
Aarav Arora <45216129+aaravarora01@users.noreply.github.com>
2022-11-03 09:26:48 -0700

    final flashcards

--- src/main/java/com/nighthawk/spring_portfolio/mvc/QuizMe/QuizMeApplication.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/QuizMe/QuizMeApplication.java
@@ -21,33 +21,35 @@ public class QuizMeApplication {
     @Autowired
     private QuizMeJpaRepository repository;
 
-   @GetMapping("/test2")
+   @GetMapping("/test")
     public ResponseEntity<List<FlashcardSets>> getFlashcard () {
         return new ResponseEntity<>( repository.findAll(), HttpStatus.OK);
 		
     }
 
-	@PostMapping("/new2")
+	@PostMapping("/new")
 	public ResponseEntity<FlashcardSets> createFlashcard () {
-		Map <String, String> flashcards = new HashMap<>();
-			flashcards.put("Accessor Method", "a method that accesses an object but does not change it, actual parameter - the expression supplied by the caller");
-			flashcards.put("Binary", "the binary, or the base-2, positional numbering system represents numerical values using only two symbols, 0 and 1");
-			flashcards.put("Boolean Operator", "an operator that can be applied to Boolean values. Java has 3 boolean, or logical, operators: &&, ||, and !.");
-			flashcards.put("Cast", "Explicitly converting a value from one type to a different type");
-			flashcards.put("Class", "a programmer defined data type, may be a blueprint or template for creating an object");
-			flashcards.put("Constructor", "A set of statements for initializing a newly instantiated variable");
-			flashcards.put("Formal parameter", "a variable in a method definition, initialized with an actual parameter value when the method is called");
-			flashcards.put("Implicit Parameter", "The object on which a method is invoked.");
-			flashcards.put("Instance variable", "a variable defined in a class for which every object of the class has its own value.");
-			flashcards.put("Loop", "a sequence of instructions that is executed repeatedly");
-			flashcards.put("Object", "a value of a class type");
-			flashcards.put("Object Oriented Programming", "designing a program by discovering objects, their properties, and their relationships");
-			flashcards.put("Primitives", "a data type structured by Java to hold a single data item (Integer, character, floating point, true/false, long)");
-			flashcards.put("Syntax", "rules that define how to form instructions in a particular programming language");
-			FlashcardSets APCSASet = new FlashcardSets();
-			APCSASet.setName("APCSA Set");
-			APCSASet.setFlashcards(flashcards);
-			repository.save(APCSASet);
-			return new ResponseEntity<>(APCSASet, HttpStatus.OK);
+		ArrayList<Flashcard> flashcards = new ArrayList<>();
+		long id = 0;
+
+		flashcards.add(new Flashcard(id++, "Accessor Method", "a method that accesses an object but does not change it, actual parameter - the expression supplied by the caller"));
+		flashcards.add(new Flashcard(id++, "Binary", "the binary, or the base-2, positional numbering system represents numerical values using only two symbols, 0 and 1"));
+		flashcards.add(new Flashcard(id++, "Boolean Operator", "an operator that can be applied to Boolean values. Java has 3 boolean, or logical, operators: &&, ||, and !."));
+		flashcards.add(new Flashcard(id++, "Cast", "Explicitly converting a value from one type to a different type"));
+		flashcards.add(new Flashcard(id++, "Class", "a programmer defined data type, may be a blueprint or template for creating an object"));
+		flashcards.add(new Flashcard(id++, "Constructor", "A set of statements for initializing a newly instantiated variable"));
+		flashcards.add(new Flashcard(id++, "Formal parameter", "a variable in a method definition, initialized with an actual parameter value when the method is called"));
+		flashcards.add(new Flashcard(id++, "Implicit Parameter", "The object on which a method is invoked."));
+		flashcards.add(new Flashcard(id++, "Instance variable", "a variable defined in a class for which every object of the class has its own value."));
+		flashcards.add(new Flashcard(id++, "Loop", "a sequence of instructions that is executed repeatedly"));
+		flashcards.add(new Flashcard(id++, "Object", "a value of a class type"));
+		flashcards.add(new Flashcard(id++, "Object Oriented Programming", "designing a program by discovering objects, their properties, and their relationships"));
+		flashcards.add(new Flashcard(id++, "Primitives", "a data type structured by Java to hold a single data item (Integer, character, floating point, true/false, long)"));
+		flashcards.add(new Flashcard(id++, "Syntax", "rules that define how to form instructions in a particular programming language"));
+		FlashcardSets APCSASet = new FlashcardSets();
+		APCSASet.setName("APCSA Set");
+		APCSASet.setFlashcards(flashcards);
+		repository.save(APCSASet);
+		return new ResponseEntity<>(APCSASet, HttpStatus.OK);
 	}
 }
